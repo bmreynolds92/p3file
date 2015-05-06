1fda4dbeadeab8adb43f3e4f6f218e4c008a37db
ajjl <ajjleinoff@gmail.com>
2015-05-05 22:46:30 -0500

    deleted all

--- work/sisc/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity sisc is
-    port(
-        CLK             : in     vl_logic;
-        RST_F           : in     vl_logic
-    );
-end sisc;
--- work/sisc_tb/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity sisc_tb is
-end sisc_tb;
